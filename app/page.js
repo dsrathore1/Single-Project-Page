f1ae2f0086c1aa7ce01ebcc8d48ed83cb8f195af
DS Rathore <dsrathorebsw1234@gmail.com>
2024-06-14 18:32:30 +0530

    Modified some files and add .env file

--- app/page.js
+++ app/page.js
@@ -2,6 +2,9 @@ export default function Home() {
   return (
     <>
       <h1 className="text-center text-4xl uppercase mt-4 font-bold">Hello</h1>
+      <div>
+        
+      </div>
     </>
   );
 }
